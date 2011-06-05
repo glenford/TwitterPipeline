644e3b2da338d7d07ad13d49e26d3bd1f18db6e6
Glen Ford <glen.a.ford@gmail.com>
2011-06-05 20:50:39 +0100

    added latest scct version

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -9,7 +9,8 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.3-SNAPSHOT"
 
   val scctRepo = "scct-repo" at "http://mtkopone.github.com/scct/maven-repo/"
-  lazy val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
+  lazy val scctPlugin = "reaktor" % "scct-sbt-for-2.9" % "0.1-SNAPSHOT"
+
 
   val akkaRepo = "Akka Repo" at "http://akka.io/repository"
   val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1"
